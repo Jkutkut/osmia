51b2ee17e7acfe5ab1085a14ca2851a8593253b9
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-11-22 20:21:56 +0100

    basic logic created

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,68 @@
 #[cfg(test)]
 mod tests;
+
+use std::collections::HashMap;
+use std::fmt::Display;
+
+
+pub fn render<T>(
+	template: &str,
+	ctx: &HashMap<&str, T>
+) -> String
+where
+	T: Display
+{
+	let syntax = Vec::from([
+		["${", "}"]
+	]);
+	builder::<T>(template, syntax, &|block| {
+		// TODO handle invalid types
+		ctx.get(block).unwrap().to_string()
+	})
+}
+
+fn builder<T>(
+	template: &str,
+	syntax_blocks: Vec<[&str; 2]>,
+	on_block: &dyn Fn(&str) -> String
+) -> String
+where
+	T: Display
+{
+	let mut result = String::new();
+	let mut i = 0;
+	let mut last_copy = 0;
+	
+
+	// search for blocks
+	while i < template.len() {
+		let mut found = false;
+		for block in &syntax_blocks {
+			let block_start = block[0];
+			let block_end = block[1];
+
+			if template[i..].starts_with(block_start) {
+				let block_start_index = i;
+				i += block_start.len();
+				let block_end_index = template[i..].find(block_end);
+				if let Some(block_end_index) = block_end_index {
+					let block_end_index = block_end_index + i;
+					result.push_str(&template[last_copy..block_start_index]);
+					let block = &template[
+						block_start_index + block_start.len()..block_end_index
+					];
+					result.push_str(on_block(block).as_str());
+					i = block_end_index + block_end.len();
+					last_copy = i;
+					found = true;
+					break;
+				}
+			}
+		}
+		if !found {
+			i += 1;
+		}
+	}
+	result.push_str(&template[last_copy..]);
+	result
+}
--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,45 @@
+use crate::render;
+
+use std::collections::HashMap;
+
 #[test]
-fn basic_test() {
-	assert_eq!(1, 1);
+fn identity() {
+	let template = "Hello, world!";
+	let cxt: HashMap<&str, String> = HashMap::new();
+	let expected = "Hello, world!";
+	let actual = render(template, &cxt);
+	assert_eq!(expected, actual);
 }
 
+#[test]
+fn basic_test() {
+	let template = "Hello, ${name}!";
+	let cxt = std::collections::HashMap::from([
+		("name", "world")
+	]);
+	let expected = "Hello, world!";
+	let actual = render(template, &cxt);
+	assert_eq!(expected, actual);
+}
+
+#[test]
+fn fail_render() {
+	let template = "Hello, ${name}!";
+	let cxt = std::collections::HashMap::from([
+		("name", 1)
+	]);
+	let expected = "Hello, world!";
+	let actual = render(template, &cxt);
+	assert_ne!(expected, actual);
+}
+
+#[test]
+fn not_found_key() {
+	let template = "Hello, ${name}!";
+	let cxt = std::collections::HashMap::from([
+		("name2", "world")
+	]);
+	let expected = "Hello, world!";
+	let actual = render(template, &cxt);
+	assert_ne!(expected, actual);
+}
