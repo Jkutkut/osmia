69c72d5e1cfbef3cf2be78d1b27cdfcac4defc52
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-11-24 18:00:40 +0100

    let's implement the logic to handle more cases

--- src/lib.rs
+++ src/lib.rs
@@ -4,43 +4,41 @@ mod tests;
 use std::collections::HashMap;
 use std::fmt::Display;
 
-
 pub fn render<T>(
 	template: &str,
 	ctx: &HashMap<&str, T>
-) -> String
+) -> Result<String, String>
 where
 	T: Display
 {
-	let syntax = Vec::from([
-		["${", "}"]
+	let blocks = Vec::from([
+		["{{{", "}}}"],
+		["{{", "}}"]
 	]);
-	builder::<T>(template, syntax, &|block| {
-		// TODO handle invalid types
-		ctx.get(block).unwrap().to_string()
+	builder::<T>(template, blocks, &|block| {
+		match ctx.get(block) {
+			Some(value) => Ok(value.to_string()),
+			None => Err(format!("Key not found: {}", block))
+		}
 	})
 }
 
 fn builder<T>(
 	template: &str,
 	syntax_blocks: Vec<[&str; 2]>,
-	on_block: &dyn Fn(&str) -> String
-) -> String
+	on_block: &dyn Fn(&str) -> Result<String, String>
+) -> Result<String, String>
 where
 	T: Display
 {
 	let mut result = String::new();
 	let mut i = 0;
 	let mut last_copy = 0;
-	
-
-	// search for blocks
 	while i < template.len() {
 		let mut found = false;
 		for block in &syntax_blocks {
 			let block_start = block[0];
 			let block_end = block[1];
-
 			if template[i..].starts_with(block_start) {
 				let block_start_index = i;
 				i += block_start.len();
@@ -51,7 +49,11 @@ where
 					let block = &template[
 						block_start_index + block_start.len()..block_end_index
 					];
-					result.push_str(on_block(block).as_str());
+					let block = match on_block(block) {
+						Ok(block) => block,
+						Err(err) => return Err(err)
+					};
+					result.push_str(block.as_str());
 					i = block_end_index + block_end.len();
 					last_copy = i;
 					found = true;
@@ -64,5 +66,5 @@ where
 		}
 	}
 	result.push_str(&template[last_copy..]);
-	result
+	Ok(result)
 }
--- src/tests.rs
+++ src/tests.rs
@@ -7,39 +7,60 @@ fn identity() {
 	let template = "Hello, world!";
 	let cxt: HashMap<&str, String> = HashMap::new();
 	let expected = "Hello, world!";
-	let actual = render(template, &cxt);
-	assert_eq!(expected, actual);
+	match render(template, &cxt) {
+		Ok(actual) => assert_eq!(expected, actual),
+		Err(err) => panic!("{}", err)
+	}
 }
 
 #[test]
 fn basic_test() {
-	let template = "Hello, ${name}!";
+	let template = "Hello, {{name}}!";
 	let cxt = std::collections::HashMap::from([
 		("name", "world")
 	]);
 	let expected = "Hello, world!";
-	let actual = render(template, &cxt);
-	assert_eq!(expected, actual);
+	match render(template, &cxt) {
+		Ok(actual) => assert_eq!(expected, actual),
+		Err(err) => panic!("{}", err)
+	}
 }
 
 #[test]
 fn fail_render() {
-	let template = "Hello, ${name}!";
+	let template = "Hello, {{name}}!";
 	let cxt = std::collections::HashMap::from([
 		("name", 1)
 	]);
 	let expected = "Hello, world!";
-	let actual = render(template, &cxt);
-	assert_ne!(expected, actual);
+	match render(template, &cxt) {
+		Ok(actual) => assert_ne!(expected, actual),
+		Err(err) => assert_eq!(err, "Key not found: name")
+	}
 }
 
 #[test]
 fn not_found_key() {
-	let template = "Hello, ${name}!";
+	let template = "Hello, {{name}}!";
 	let cxt = std::collections::HashMap::from([
 		("name2", "world")
 	]);
 	let expected = "Hello, world!";
-	let actual = render(template, &cxt);
-	assert_ne!(expected, actual);
+	match render(template, &cxt) {
+		Ok(actual) => assert_ne!(expected, actual),
+		Err(err) => assert_eq!(err, "Key not found: name")
+	}
+}
+
+#[test]
+fn similar_syntax() {
+	let template = "Hello, {{name}}! The syntax to us a variable is {{{name}}}.";
+	let cxt = std::collections::HashMap::from([
+		("name", "world")
+	]);
+	let expected = "Hello, world! The syntax to us a variable is {{name}}.";
+	match render(template, &cxt) {
+		Ok(actual) => assert_eq!(expected, actual),
+		Err(err) => panic!("{}", err)
+	}
 }
