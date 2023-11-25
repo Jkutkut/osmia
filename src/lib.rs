40b18aa01f62ac720d719a5388675c228ba97d71
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-11-25 16:01:54 +0100

    refactor: API simplified to allow more flexible and simplier use

--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,12 @@
 //!
 //! ## Usage
 //! ```rust
-//! use osmia::{osmia_syntax, RenderSyntax};
-//! use std::collections::HashMap;
+//! use osmia::{osmia_syntax, RenderSyntax, Json};
 //!
 //! let template = "Hello, {{name}}!";
 //! let result = osmia_syntax().render(
 //!		"Hello, {{name}}!",
-//!		&HashMap::from([("name", "world")])
+//!		&serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap()
 //!	).unwrap();
 //! assert_eq!(result, "Hello, world!".to_string());
 //! ```
@@ -24,12 +23,13 @@
 #[cfg(test)]
 mod tests;
 
-use std::collections::HashMap;
-use std::fmt::Display;
+/// Alias for the serde_json::Value type.
+///
+/// It's used as the context of the template.
+pub type Json = serde_json::Value;
 
-/// Alias for the context of the template.
-/// It's a HashMap with a string as key and anything implementing the trait Display.
-type OsmiaContext<'a, T> = &'a HashMap<&'a str, T>;
+/// Internal type to handle the context of the template.
+type JsonRef<'a> = &'a Json;
 
 /// Internal type to handle the result of the render function.
 ///
@@ -41,23 +41,44 @@ type OsmiaContext<'a, T> = &'a HashMap<&'a str, T>;
 /// If there's an error, it returns the error as a `String`.
 pub type OsmiaResult = Result<Option<(String, usize)>, String>;
 
-/// Type of the functions detecting the syntax blocks.
-type FtStractBlock<T> = dyn Fn(&str, OsmiaContext<T>) -> OsmiaResult;
+/// Type of the functions used to detect simple blocks.
+///
+/// A simple block is a block without any logic, like a comment or a variable.
+///
+/// # Arguments
+///
+/// * `code` - The code to parse.
+///
+/// * `ctx` - The context of the template.
+type FtSimpleBlock = dyn Fn(&str, JsonRef) -> OsmiaResult;
+
+/// Type of the functions used to detect complex blocks.
+///
+/// A complex block is a block with logic, like a for loop.
+///
+/// # Arguments
+///
+/// * `code` - The code to parse.
+///
+/// * `ctx` - The context of the template.
+///
+/// * `syntax` - The syntax of the template.
+type FtComplexBlock = dyn Fn(&str, JsonRef, &RenderSyntax) -> OsmiaResult;
 
 /// Struct to store the syntax of the template.
-pub struct RenderSyntax<'a, T> {
-	simple_blocks: Vec<&'a FtStractBlock<T>>,
-	//complex_blocks: Vec<()>, // TODO
+///
+/// It stores the functions used to detect the blocks.
+pub struct RenderSyntax<'a> {
+	simple_blocks: Vec<&'a FtSimpleBlock>,
+	complex_blocks: Vec<&'a FtComplexBlock>
 }
 
-impl<'a, T> RenderSyntax<'a, T> {
-	/// Create a new RenderSyntax.
-	///
-	/// It follows the Builder pattern.
+impl<'a> RenderSyntax<'a> {
+	/// Create a new RenderSyntax struct.
 	pub fn new() -> Self {
 		return RenderSyntax {
 			simple_blocks: Vec::new(),
-			// complex_blocks: Vec::new()
+			complex_blocks: Vec::new()
 		};
 	}
 
@@ -73,7 +94,7 @@ impl<'a, T> RenderSyntax<'a, T> {
 	/// ```
 	/// use osmia::{osmia_syntax, RenderSyntax};
 	///
-	/// let mut syntax: RenderSyntax<'_, String> = RenderSyntax::new();
+	/// let mut syntax = RenderSyntax::new();
 	/// syntax.add_simple_block(&|code, _ctx| {
 	///		// Logic
 	///		Ok(None)
@@ -85,9 +106,17 @@ impl<'a, T> RenderSyntax<'a, T> {
 	///	```
 	pub fn add_simple_block(
 		&mut self,
-		render_function: &'a FtStractBlock<T>
+		render_function: &'a FtSimpleBlock
 	) -> &mut Self {
 		self.simple_blocks.push(render_function);
+		self
+	}
+
+	pub fn add_complex_block(
+		&mut self,
+		render_function: &'a FtComplexBlock
+	) -> &mut Self {
+		self.complex_blocks.push(render_function);
 		self
 	}
 
@@ -100,30 +129,43 @@ impl<'a, T> RenderSyntax<'a, T> {
 	/// * `ctx` - The context of the template.
 	///
 	/// # Example
-	///
 	/// ```rust
-	/// use osmia::osmia_syntax;
-	/// use std::collections::HashMap;
+	/// use osmia::{osmia_syntax, Json};
 	///
 	/// let result = osmia_syntax().render(
-	///		"Hello, {{name}}!",
-	/// 	&HashMap::from([("name", "world")])
+	/// 	"Hello, {{name}}!",
+	/// 	&serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap()
 	/// ).unwrap();
 	/// assert_eq!(result, "Hello, world!".to_string());
 	/// ```
 	pub fn render(
 		self,
 		template: &str,
-		ctx: OsmiaContext<T>
-	) -> Result<String, String>
-	where
-		T: Display
-	{
+		ctx: JsonRef
+	) -> Result<String, String> {
+		match ctx.as_object() {
+			Some(ctx) => ctx,
+			None => return Err("Context should be an object".to_string())
+		};
 		let mut result = String::new();
 		let mut i = 0;
 		let mut last_copy = 0;
 		while i < template.len() {
 			let mut found = false;
+			for render_function in &self.complex_blocks {
+				match render_function(&template[i..], ctx, &self) { // TODO refactor
+					Ok(None) => {},
+					Ok(Some((block, block_size))) => {
+						result.push_str(&template[last_copy..i]);
+						result.push_str(block.as_str());
+						i += block_size;
+						last_copy = i;
+						found = true;
+						break;
+					},
+					Err(err) => return Err(err)
+				}
+			}
 			for render_function in &self.simple_blocks {
 				match render_function(&template[i..], ctx) {
 					Ok(None) => {},
@@ -159,21 +201,60 @@ impl<'a, T> RenderSyntax<'a, T> {
 ///
 /// # Example
 /// ```
-/// use osmia::{osmia_syntax, RenderSyntax};
-/// use std::collections::HashMap;
+/// use osmia::{osmia_syntax, RenderSyntax, Json};
 ///
 /// let template = "Hello, {{name}}!";
-/// let cxt = HashMap::from([("name", "world")]);
+/// let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
 /// let expected = "Hello, world!".to_string();
 /// let result = osmia_syntax().render(template, &cxt).unwrap();
 /// assert_eq!(result, expected);
 /// ```
-pub fn osmia_syntax<'a, T>() -> RenderSyntax<'a, T>
-where
-	T: Display
-{
-	let mut syntax: RenderSyntax<'_, T> = RenderSyntax::new();
-	syntax.add_simple_block(&|code, _ctx| {
+pub fn osmia_syntax<'a>() -> RenderSyntax<'a> {
+	let mut syntax: RenderSyntax<'_> = RenderSyntax::new();
+	syntax
+	/*.add_complex_block(&|code, ctx, syntax| {
+		let result = match detect_simple_block("{{for", "}}", code) {
+			Ok(None) => return Ok(None),
+			Err(err) => return Err(err),
+			Ok(Some((block, block_size))) => Ok(Some((block, block_size)))
+		};
+		let (block, block_size) = result.clone().unwrap().unwrap();
+		let block_arr = block.trim().split_whitespace().collect::<Vec<&str>>();
+		if block_arr.len() != 3 || block_arr[1] != "in" {
+			return Err(format!("Invalid for block: {}", block));
+		}
+		let key = block_arr[0];
+		let value = match ctx.get(block_arr[2]) {
+			Some(value) => value,
+			None => return Err(format!("Key not found: {}", block_arr[2]))
+		};
+		let iterator = match value.as_array() {
+			Some(iterator) => iterator,
+			None => return Err(format!("\"{}\" is not iterable", block_arr[2]))
+		};
+		let mut i = block_size;
+		while i < code.len() && !code[i..].starts_with("{{end}}") {
+			i += 1;
+		}
+		if i >= code.len() || !code[i..].starts_with("{{end}}") {
+			return Err(format!("Missing closing block: {{end}}"));
+		}
+		let new_code = &code[block_size..i];
+		let mut new_ctx = ctx.clone();
+		new_ctx.insert(key, value);
+		let mut new_block = String::new();
+		for (i, value) in iterator.iter().enumerate() {
+			match syntax.render(new_code, ctx) {
+				Err(err) => return Err(err),
+				Ok(rendered) => new_block.push_str(rendered.as_str())
+			}
+		}
+
+		let new_block_size = i + "{{end}}".len();
+		let result = Ok(Some((new_block, new_block_size)));
+		result
+	})*/
+	.add_simple_block(&|code, _ctx| {
 		let result = detect_simple_block("{*", "*}", code);
 		if let Ok(Some((_block, block_size))) = &result {
 			return Ok(Some((String::new(), *block_size)));
@@ -192,7 +273,10 @@ where
 		let result = detect_simple_block("{{", "}}", code);
 		if let Ok(Some((block, block_size))) = &result {
 			let value = match ctx.get(block.as_str()) {
-				Some(value) => value.to_string(),
+				Some(value) => match value.as_str() {
+					Some(value) => value.to_owned(),
+					None => return Err(format!("\"{}\" is not a string", block))
+				},
 				None => return Err(format!("Key not found: {}", block))
 			};
 			return Ok(Some((value, *block_size)));
@@ -212,10 +296,11 @@ where
 ///
 /// # Example
 /// ```
-/// use osmia::{detect_simple_block, RenderSyntax, OsmiaResult};
+/// use osmia::{detect_simple_block, RenderSyntax, OsmiaResult, Json};
 /// use std::collections::HashMap;
+/// use serde_json::Map;
 ///
-/// let mut syntax: RenderSyntax<'_, String> = RenderSyntax::new();
+/// let mut syntax = RenderSyntax::new();
 /// syntax.add_simple_block(&|code, _ctx| {
 /// 	let result = detect_simple_block("{*", "*}", code);
 /// 	if let Ok(Some((_block, block_size))) = &result {
@@ -223,11 +308,10 @@ where
 /// 	}
 /// 	result
 /// });
-/// 
-/// let result = syntax.render("Hello, {* comment *}!", &HashMap::new()).unwrap();
+/// let ctx: Json = serde_json::from_str(r#"{}"#).unwrap();
+/// let result = syntax.render("Hello, {* comment *}!", &ctx).unwrap();
 /// assert_eq!(result, "Hello, !");
 /// ```
-/// 
 pub fn detect_simple_block(
 	start: &str,
 	end: &str,
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,11 @@
-use crate::osmia_syntax;
+use crate::{osmia_syntax, Json};
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
 
 #[test]
 fn identity() {
 	let template = "Hello, world!";
-	let cxt: HashMap<&str, String> = HashMap::new();
+	let cxt = Json::Object(serde_json::Map::new());
 	let expected = "Hello, world!";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
@@ -16,9 +16,7 @@ fn identity() {
 #[test]
 fn basic_test() {
 	let template = "Hello, {{name}}!";
-	let cxt = std::collections::HashMap::from([
-		("name", "world")
-	]);
+	let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
 	let expected = "Hello, world!";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
@@ -27,37 +25,67 @@ fn basic_test() {
 }
 
 #[test]
-fn fail_render() {
+fn not_found_key() {
 	let template = "Hello, {{name}}!";
-	let cxt = std::collections::HashMap::from([
-		("name", 1)
-	]);
-	let expected = "Hello, world!";
-	match osmia_syntax().render(template, &cxt) {
-		Ok(actual) => assert_ne!(expected, actual),
-		Err(err) => assert_eq!(err, "Key not found: name")
+	let ctx = serde_json::from_str::<Json>(r#"{ "name2": "world" }"#).unwrap();
+	match osmia_syntax().render(template, &ctx) {
+		Ok(actual) => panic!("Expected error, got: {}", actual),
+		// contains "key not found" and "name"
+		Err(err) => {
+			let err = err.to_string();
+			assert!(
+				err.to_lowercase().contains("key not found") &&
+				err.contains("name")
+			)
+		}
 	}
 }
 
 #[test]
-fn not_found_key() {
+fn invalid_ctx() {
 	let template = "Hello, {{name}}!";
-	let cxt = std::collections::HashMap::from([
-		("name2", "world")
-	]);
-	let expected = "Hello, world!";
-	match osmia_syntax().render(template, &cxt) {
-		Ok(actual) => assert_ne!(expected, actual),
-		Err(err) => assert_eq!(err, "Key not found: name")
+	let ctx = serde_json::from_str::<Json>(r#"[]"#).unwrap();
+	match osmia_syntax().render(template, &ctx) {
+		Ok(actual) => panic!("Expected error, got: {}", actual),
+		Err(err) => {
+			let err = err.to_string();
+			assert!(err.contains("should be"));
+			assert!(err.contains("object"));
+		}
+	}
+	let ctx = serde_json::from_str::<Json>(r#"null"#).unwrap();
+	match osmia_syntax().render(template, &ctx) {
+		Ok(actual) => panic!("Expected error, got: {}", actual),
+		Err(err) => {
+			let err = err.to_string();
+			assert!(err.contains("should be"));
+			assert!(err.contains("object"));
+		}
+	}
+	let ctx = serde_json::from_str::<Json>(r#"true"#).unwrap();
+	match osmia_syntax().render(template, &ctx) {
+		Ok(actual) => panic!("Expected error, got: {}", actual),
+		Err(err) => {
+			let err = err.to_string();
+			assert!(err.contains("should be"));
+			assert!(err.contains("object"));
+		}
+	}
+	let ctx = serde_json::from_str::<Json>(r#"1"#).unwrap();
+	match osmia_syntax().render(template, &ctx) {
+		Ok(actual) => panic!("Expected error, got: {}", actual),
+		Err(err) => {
+			let err = err.to_string();
+			assert!(err.contains("should be"));
+			assert!(err.contains("object"));
+		}
 	}
 }
 
 #[test]
 fn similar_syntax() {
 	let template = "Hello, {{name}}! The syntax is \"Hello, {{{name}}}!\".";
-	let cxt = std::collections::HashMap::from([
-		("name", "world")
-	]);
+	let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
 	let expected = "Hello, world! The syntax is \"Hello, {{name}}!\".";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
@@ -67,11 +95,9 @@ fn similar_syntax() {
 
 #[test]
 fn comment() {
-	let template = "Hello, {{name}}!{* This is a comment *}";
-	let cxt = std::collections::HashMap::from([
-		("name", "world")
-	]);
-	let expected = "Hello, world!";
+	let template = "Hello!{* This is a comment *}";
+	let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
+	let expected = "Hello!";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
 		Err(err) => panic!("{}", err)
@@ -80,11 +106,9 @@ fn comment() {
 
 #[test]
 fn comment_with_brackets() {
-	let template = "Hello, {{name}}!{* This is a comment with brackets: { { } } *}";
-	let cxt = std::collections::HashMap::from([
-		("name", "world")
-	]);
-	let expected = "Hello, world!";
+	let template = "Hello!{* This is a comment with brackets: { { } } *}";
+	let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
+	let expected = "Hello!";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
 		Err(err) => panic!("{}", err)
@@ -94,12 +118,38 @@ fn comment_with_brackets() {
 #[test]
 fn comment_with_brackets_and_text() {
 	let template = "Hello, {{name}}!{* This is a comment with brackets: {{ {{{ }}} }} *}";
-	let cxt = std::collections::HashMap::from([
-		("name", "world")
-	]);
+	let cxt = serde_json::from_str::<Json>(r#"{ "name": "world" }"#).unwrap();
 	let expected = "Hello, world!";
 	match osmia_syntax().render(template, &cxt) {
 		Ok(actual) => assert_eq!(expected, actual),
 		Err(err) => panic!("{}", err)
 	}
 }
+
+// #[test]
+// fn for_each() {
+// 	let template = "Hello, {{name}}!
+// {{for item in items}}
+// 	<a href=\"{{item.url}}\">{{item.name}}</a>
+// {{end}}";
+// 	let ctx: Json = serde_json::from_str(r#"
+// 		{
+// 			"name": "world",
+// 			"items": [
+// 				{
+// 					"url": "https://example.com",
+// 					"name": "Example"
+// 				},
+// 				{
+// 					"url": "https://example.org",
+// 					"name": "Example"
+// 				}
+// 			]
+// 		}
+// 	"#).unwrap();
+// 	let expected = "Hello, world!\n\t<a href=\"https://example.com\">Example</a>\n\t<a href=\"https://example.org\">Example</a>";
+// 	match osmia_syntax().render(template, &ctx) {
+// 		Ok(actual) => assert_eq!(expected, actual),
+// 		Err(err) => panic!("{}", err)
+// 	}
+// }
