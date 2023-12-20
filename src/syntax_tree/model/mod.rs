4068b5f80002d16951112a463f78bb493f044a2e
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2023-12-21 00:05:51 +0100

    wpi: syntax-tree full model

--- src/syntax_tree/model/mod.rs
+++ src/syntax_tree/model/mod.rs
@@ -13,3 +13,20 @@ pub use grouping::Grouping;
 pub use literal::Literal;
 pub use unary::Unary;
 pub use variable::Variable;
+
+enum Stmt<'a> {
+	Expression(Expression<'a>),
+	// Print(Expression<'a>),
+	// Var(&'a str, Option<Expression<'a>>),
+	// Block(Vec<Stmt<'a>>),
+	If(Expression<'a>, Box<Stmt<'a>>, Option<Box<Stmt<'a>>>),
+	// While(Expression<'a>, Box<Stmt<'a>>),
+	ForEach(Variable<'a>, Variable<'a>, Box<Stmt<'a>>),
+	// Break,
+	// Continue,
+}
+
+enum CodeBlock<'a> {
+	Stmt(Stmt<'a>),
+	Raw(&'a str),
+}
