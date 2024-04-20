d43b20991fb6275b50c54758a9ab2cee088e6a0e
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-04-20 22:23:09 +0200

    feat: documentation

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,178 @@ use lexer::Lexer;
 use lexer::Token;
 use model::Stmt;
 
+/// This simple API is designed to use the osmia template engine.
+///
+/// # Syntax
+/// ## Text:
+/// By default, all text outside the delimiters will be treated as a plain text.
+///
+/// ```text
+/// Hey there! This is a plain text
+/// ```
+/// ```text
+/// Hey there! This is a plain text
+/// ```
+///
+/// ## Expression:
+/// Evaluates and prints the given expression.
+/// ```text
+/// 1 + 2 = {{ 1 + 2 }}
+/// ```
+/// ```text
+/// 1 + 2 = 3
+/// ```
+///
+/// ## Assign:
+/// Assigns the given expression to the given variable in the context.
+/// ```text
+/// {{ assign foo = 1 + 2 }}
+/// The result is {{foo}}
+/// ```
+/// ```text
+/// The result is 3
+/// ```
+///
+/// ## Print:
+/// Prints the given expression in the stdin. It can be useful when debugging.
+/// ```text
+/// {{ print "Hello world!" }}
+/// ```
+/// ```text
+/// ```
+///
+/// ## If:
+/// ### Single if:
+/// ```text
+/// {{ if 1 == 1 }}
+/// Math is not broken!
+/// {{ fi }}
+/// ```
+/// ```text
+/// Math is not broken!
+/// ```
+///
+/// ### If, else:
+/// ```text
+/// {{ if 1 == 2 }}
+/// Math is not broken!
+/// {{ else }}
+/// Nope, math is right >D
+/// {{ fi }}
+/// ```
+/// ```text
+/// Nope, math is right >D
+/// ```
+///
+/// ### If, else if:
+/// ```text
+/// {{assign value = 2}}
+/// {{ if value == 1 }}
+/// Nope
+/// {{ elseif value == 2 }}
+/// The value is 2!
+/// {{ else }}
+/// Nope
+/// {{ fi }}
+/// ```
+/// ```text
+/// The value is 2!
+/// ```
+///
+/// ## While:
+/// ```text
+/// {{ assign i = 0 }}
+/// {{ while i < 5 }}
+/// {{ assign i = i + 1 }}
+/// i = {{ i }}
+/// {{done}}
+/// ```
+/// ```text
+/// i = 1
+/// i = 2
+/// i = 3
+/// i = 4
+/// i = 5
+/// ```
+///
+/// ## For:
+/// ```text
+/// {{ for i in [1, 2, 3] }}
+///   i = {{ i }}
+/// {{ done }}
+/// {{assign arr = [
+///   {"name": "Marvin1"},
+///   {"name": "Marvin2"},
+///   {"name": "Marvin3"}
+/// ]}}
+/// {{for user in arr}}
+/// User: {{ user.name }}
+/// {{done}}
+/// ```
+/// ```text
+/// i = 1
+/// i = 2
+/// i = 3
+/// User: Marvin1
+/// User: Marvin2
+/// User: Marvin3
+/// ```
+///
+/// ## Continue, break:
+/// ```text
+/// {{continue}}
+/// {{break}}
+/// ```
+///
+/// ## Advanced control:
+/// All variable in the context can be used and edited similarly to
+/// variables in JavaScript.
+/// ```text
+/// {{ assign obj = {"user": {"name": "Marvin"} } }}
+/// Old name: {{ obj.user.name }}
+/// {{ assign obj.user.name = "R2D2" }}
+/// New name: {{ obj.user.name }}
+/// ```
+/// ```text
+/// Old name: Marvin
+/// New name: R2D2
+/// ```
+///
+/// # Examples
+///
+/// ## Basic execution
+/// ```rust
+/// use osmia::Osmia;
+///
+/// let mut interpreter = Osmia::new();
+/// let code = Osmia::code("Hello world! {{ 1 + 2 }}").unwrap();
+/// let result = interpreter.run(&code).unwrap();
+/// assert_eq!(result, "Hello world! 3");
+/// ```
+///
+/// ## Context
+/// The context can be passed as a JSON object in the form of a string.
+/// ```rust
+/// use osmia::Osmia;
+///
+/// let context_json = r#"{"foo": "bar"}"#;
+/// let code = "The value of foo is {{ foo }}";
+/// let mut interpreter = Osmia::from_json(context_json).unwrap();
+/// let code = Osmia::code(code).unwrap();
+/// let result = interpreter.run(&code).unwrap();
+/// assert_eq!(result, "The value of foo is bar");
+/// ```
+///
+/// ## Experimental features: Custom delimiters
+/// You can change the delimiters used by the code lexer.
+/// ```rust
+/// use osmia::Osmia;
+///
+/// let mut interpreter = Osmia::from_json(r#"{"user": {"name": "Marvin"}}"#).unwrap();
+/// let code = Osmia::custom_code("User: ${ user.name }", "${", "}").unwrap();
+/// let result = interpreter.run(&code).unwrap();
+/// assert_eq!(result, "User: Marvin");
+/// ```
 pub struct Osmia {
 }
 
