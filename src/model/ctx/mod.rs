90cbd7445a4162852fe02407c3b067d06abe436e
jkutkut <45950243+Jkutkut@users.noreply.github.com>
2024-09-09 17:49:24 +0200

    wip: Ctx

--- src/model/ctx/mod.rs
+++ src/model/ctx/mod.rs
@@ -6,6 +6,41 @@ pub use json_tree::JsonTree;
 pub use json_tree_error::JsonTreeError;
 pub use json_tree_key::JsonTreeKey;
 
+type CtxKey = String;
+type CtxValue = String;
+
+pub struct Ctx {
+	ctx: JsonTree<CtxKey, CtxValue>,
+}
+
+impl Ctx {
+	#[allow(dead_code)]
+	pub fn new() -> Self {
+		Self { ctx: JsonTree::Object(
+			std::collections::HashMap::new()
+		) }
+	}
+
+	#[allow(dead_code)]
+	#[allow(unused_variables)]
+	pub fn get<'a>(
+		&self,
+		key: &mut impl Iterator<Item = &'a JsonTreeKey<CtxKey>>
+	) -> Result<&JsonTree<CtxKey, CtxValue>, JsonTreeError<JsonTreeKey<CtxKey>>> {
+		self.ctx.get(key)
+	}
+
+	#[allow(dead_code)]
+	#[allow(unused_variables)]
+	pub fn set<'a>(
+		&mut self,
+		key: &mut impl Iterator<Item = &'a JsonTreeKey<CtxKey>>,
+		value: CtxValue
+	) -> Result<(), JsonTreeError<JsonTreeKey<CtxKey>>> {
+		self.ctx.set(key, JsonTree::Value(value))
+	}
+}
+
 // TODO Remove once real Ctx is implemented
 use serde::Deserialize;
 
